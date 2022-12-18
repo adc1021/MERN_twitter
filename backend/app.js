bfa4b76d0b192a103f92d7863fa425e715f4db2d
Anthony_Chiodi <adc8wlc@gmail.com>
2022-12-18 16:31:01 -0500

    protected against csrf attacks

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,39 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const csurf = require("csurf");
+const cors = require("cors");
 
-const usersRouter = require('./routes/api/users'); // update the import file path
-const tweetsRouter = require('./routes/api/tweets')
+const { isProduction } = require("./config/keys");
+
+const usersRouter = require("./routes/api/users"); // update the import file path
+const tweetsRouter = require("./routes/api/tweets");
+const csrfRouter = require("./routes/api/csrf")
 
 const app = express();
 
-app.use(logger('dev')); // log request components (URL/method) to terminal
+app.use(logger("dev")); // log request components (URL/method) to terminal
 app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
 
-// Attach Express routers
-app.use('/api/users', usersRouter); // update the path
-app.use('/api/tweets', tweetsRouter);
+if (!isProduction) {
+  app.use(cors());
+}
+
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+  );
+
+  // Attach Express routers
+app.use("/api/users", usersRouter); // update the path
+app.use("/api/tweets", tweetsRouter);
+app.use("/api/csrf", csrfRouter)
 
 module.exports = app;
--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI
+  mongoURI: process.env.MONGO_URI,
+  isProduction: process.env.NOCE_ENV === 'production'
 }
-
